53e33e2b44f03113d1ba584f4c12450eae590276
liushiquan <liushiquan@meicai.cn>
2020-06-02 15:28:31 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+import VueAliplayerV2 from 'vue-aliplayer-v2';
+
+Vue.use(VueAliplayerV2);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/movie/index.vue
+++ src/views/movie/index.vue
@@ -1,15 +1,125 @@
 <template>
- <div>
-     hello movie
- </div>
+    <div id="app">
+        <template v-if="show">
+            <vue-aliplayer-v2 :source="source" ref="VueAliplayerV2" :options="options" />
+        </template>
+        <p class="remove-text" v-else>播放器已销毁!</p>
+<!--        <div class="player-btns">-->
+<!--            <span @click="play()">播放</span>-->
+<!--            <span @click="pause()">暂停</span>-->
+<!--            <span @click="replay()">重播</span>-->
+<!--            <span @click="getCurrentTime()">播放时刻</span>-->
+<!--            <span @click="show = !show">{{ show ? '销毁' : '重载' }}</span>-->
+<!--            <span @click="options.isLive = !options.isLive">{{ options.isLive ? '切换普通模式' : '切换直播模式' }}</span>-->
+<!--            <span @click="getStatus()">获取播放器状态</span>-->
+<!--        </div>-->
+        <div class="source-box">
+<!--            <span class="source-label">选择播放源(支持动态切换):</span>-->
+            <select v-model="source" name="source" id="source">
+                <option value="//player.alicdn.com/video/aliyunmedia.mp4">播放源1</option>
+                <option value="//yunqivedio.alicdn.com/user-upload/nXPDX8AASx.mp4">播放源2</option>
+                <option value="//tbm-auth.alicdn.com/e7qHgLdugbzzKh2eW0J/kXTgBkjvNXcERYxh2PA@@hd_hq.mp4?auth_key=1584519814-0-0-fc98b2738f331ff015f7bf5c62394888">播放源3</option>
+                <option value="//ivi.bupt.edu.cn/hls/cctv1.m3u8">直播播放源4</option>
+            </select>
+        </div>
+        <div class="source-box">
+            <span class="source-label">输入播放源(支持动态切换):</span>
+            <input class="source-input" type="text" v-model="source">
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
+    import VueAliplayerV2 from 'vue-aliplayer-v2';
+    components:{ VueAliplayerV2: VueAliplayerV2.Player }
 
+    export default {
+        data() {
+            return {
+                options: {
+                    source:'//player.alicdn.com/video/aliyunmedia.mp4'
+                },
+                source: '//player.alicdn.com/video/aliyunmedia.mp4',
+                show: true
+
+            };
+        },
+        computed: {
+            player() {
+                return this.$refs.videoPlayer.player
+            }
+        },
+        methods:{
+            play(){
+                this.$refs.VueAliplayerV2.play()
+            },
+
+            pause(){
+                this.$refs.VueAliplayerV2.pause();
+            },
+
+            replay(){
+                this.$refs.VueAliplayerV2.replay();
+            },
+
+            getCurrentTime(){
+                this.$refs.VueAliplayerV2.getCurrentTime();
+            }
+        },
+        mounted() {
+            this.$nextTick(() => {
+                // this.getVideo();
+            });
+        },
     }
 </script>
 
-<style>
-
+<style lang="less">
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    .remove-text{
+        text-align: center;
+        padding: 20px;
+        font-size: 24px;
+    }
+    .player-btns{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        span {
+            margin: 0 auto;
+            display: inline-block;
+            padding: 5px 10px;
+            width: 150px;
+            height: 40px;
+            line-height: 40px;
+            border: 1px solid #eee;
+            background: #e1e1e1;
+            border-radius: 10px;
+            text-align: center;
+            margin: 5px;
+            cursor: pointer;
+        }
+    }
+    .source-box{
+        padding: 5px 10px;
+        margin-bottom: 20px;
+        .source-label{
+            margin-right: 20px;
+            font-size: 16px;
+            display: block;
+        }
+        #source{
+            margin-top: 10px;
+        }
+        .source-input{
+            margin-top: 10px;
+            padding: 5px 10px;
+            width: 80%;
+            border: 1px solid #ccc;
+        }
+    }
 </style>
